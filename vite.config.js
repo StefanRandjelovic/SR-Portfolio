cef377bfc7b190fe2dc885c2eb6f527904664268
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-03-21 21:47:18 +0100

    Add import aliases for the created folders.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      { find: "@pages", replacement: "/src/Pages/" },
+      { find: "@components", replacement: "/src/Components/" },
+      { find: "@images", replacement: "/src/Images/" },
+      { find: "@styles", replacement: "/src/Styles/" },
+      { find: "@svg", replacement: "/src/Svg/" },
+      // {find:'', replacement:''},
+    ],
+  },
+});
