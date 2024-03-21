7a0e502ea6563d2c26ccefa65cbd5e1fac6aebfc
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-03-21 21:48:26 +0100

    Add Routes to the document via "react-router".

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+// DEV DEPENDENCIES
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// PAGES
+import Home from "@pages/Home";
+
+// COMPONENTS
+import NavBar from "@components/NavBar";
+
 function App() {
   return (
-    <>
-      <div>App</div>
-    </>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={""} />
+        <Route path="/skills" element={""} />
+        <Route path="/contact" element={""} />
+        <Route path="*" element={""} />
+      </Routes>
+    </Router>
   );
 }
 
