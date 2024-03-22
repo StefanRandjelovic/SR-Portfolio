4ad9aa537eed317e251e585c4aff7372cee0f4ba
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-03-22 18:35:00 +0100

    Make content dynamic and add global states for language and dark mode.

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,22 +1,53 @@
 // DEV DEPENDENCIES
 import { Link } from "react-router-dom";
+import { useAtom } from "jotai";
 
 // SVG Assets
 import SrLogo from "@svg/SR.svg";
+import Dark from "@svg/dark_mode.svg";
+import Light from "@svg/light_mode.svg";
 
 // STYLES
 import "@styles/NavBar.scss";
 
+// INFORMATION
+import { engInfo, srbInfo } from "@info/info-array";
+
+// GSVariable
+import { languageChange, darkMode } from "@global/store.js";
+
 const NavBar = () => {
+  // GSVariable
+  const [darkModeState, setDarkModeState] = useAtom(darkMode);
+  const [langChange, setLangChange] = useAtom(languageChange);
+
   return (
     <nav>
       <Link id="logo" to={"/"} title="Home page">
         <img src={SrLogo} alt="logo" />
       </Link>
-      <div className="right-side">
-        <Link to={"/about"}>About</Link>
-        <Link to={"/skills"}>Skills</Link>
-        <Link to={"/contact"}>Contact</Link>
+      <div id="nonLogo">
+        <div className="left-side">
+          <p onClick={() => setLangChange(false)}>{engInfo.nav[0]}</p>
+          <p onClick={() => setLangChange(true)}>{engInfo.nav[1]}</p>
+        </div>
+        <div className="right-side">
+          <Link to={"/about"}>
+            {langChange ? engInfo.nav[2] : srbInfo.nav[0]}
+          </Link>
+          <Link to={"/skills"}>
+            {langChange ? engInfo.nav[3] : srbInfo.nav[1]}
+          </Link>
+          <Link to={"/contact"}>
+            {langChange ? engInfo.nav[4] : srbInfo.nav[2]}
+          </Link>
+        </div>
+        <img
+          onClick={() => setDarkModeState(!darkModeState)}
+          src={darkModeState ? Dark : Light}
+          alt="Dark/Light mode toggle"
+          id="darkMode"
+        />
       </div>
     </nav>
   );
