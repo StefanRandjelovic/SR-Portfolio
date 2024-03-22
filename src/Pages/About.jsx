09dd46e76a39e10118cd375e5d516ae30aca6765
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-03-22 23:15:07 +0100

    Add styles and dynamic content into the About page.

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,3 +1,6 @@
+// STYLES
+import "@styles/About.scss";
+
 // INFORMATION
 import { engInfo, srbInfo } from "@info/info-array";
 
@@ -7,6 +10,9 @@ import { useAtomValue } from "jotai";
 // GSVariable
 import { languageChange, darkMode } from "@global/store.js";
 
+// IMG import
+import MyIMG from "@images/My_img.png";
+
 const About = () => {
   // GSVariable
   const darkModeState = useAtomValue(darkMode);
@@ -14,7 +20,19 @@ const About = () => {
 
   return (
     <main>
-      <h1>{langChange ? engInfo.nav[2] : srbInfo.nav[0]}</h1>
+      <div className="leftSideAbout">
+        <img src={MyIMG} alt="My image" id="myIMG" />
+        <label htmlFor="#myIMG">
+          {langChange ? engInfo.aboutImg : srbInfo.aboutImg}
+        </label>
+      </div>
+      <div className="rightSideAbout">
+        <h1>{langChange ? engInfo.nav[2] : srbInfo.nav[0]}</h1>
+        <p>{langChange ? engInfo.about[0] : srbInfo.about[0]}</p>
+        <p>{langChange ? engInfo.about[1] : srbInfo.about[1]}</p>
+        <p>{langChange ? engInfo.about[2] : srbInfo.about[2]}</p>
+        <p>{langChange ? engInfo.about[3] : srbInfo.about[3]}</p>
+      </div>
     </main>
   );
 };
