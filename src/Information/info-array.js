96d6b9e0fdde9c273ad32e2efc5c5afd8db95d63
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-03-22 18:37:38 +0100

    Make an array with the necessary information.

---
+++ src/Information/info-array.js
@@ -0,0 +1,27 @@
+const engInfo = {
+  nav: ["srb", "eng", "About", "Skills", "Contact"],
+  home: [
+    "Well hello there,",
+    "My name is Stefan",
+    "And I love to create wonderful digital things like this.",
+    "CV click here",
+  ],
+  about: [],
+  skills: [],
+  contact: [],
+};
+
+const srbInfo = {
+  nav: ["O meni", "Veštine", "Kontakt"],
+  home: [
+    "Pozdrav,",
+    "Zovem se Stefan",
+    "I volim da pravim lepe digitalne kreacije kao što je ova.",
+    "CV kilikni ovde",
+  ],
+  about: [],
+  skills: [],
+  contact: [],
+};
+
+export { engInfo, srbInfo };
