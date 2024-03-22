18790f7f5bfa7d00654241ad13ade85553d40420
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-03-22 18:39:36 +0100

    Redo the Home page to include dynamical language change and dark mode.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,18 +1,38 @@
 // STYLES
 import "@styles/Home.scss";
 
-// SVG Assets
-import SrCartoon from "@svg/Cartoonify.svg";
+// DEV DEPENDENCIES
+import { useAtomValue } from "jotai";
+
+// GSVariable
+import { languageChange, darkMode } from "@global/store.js";
+
+// IMG ASSETS
+import Avatar from "@images/avatarR.jpg";
+
+// INFORMATION
+import { engInfo, srbInfo } from "@info/info-array";
+
+import CV from "@documents/SR_CV.pdf";
 
 const Home = () => {
+  // GSVariable
+  const darkModeState = useAtomValue(darkMode);
+  const langChange = useAtomValue(languageChange);
+
   return (
     <>
-      <main>
-        <img id="avatar" src={SrCartoon} alt="" />
+      <main className={darkModeState ? "dark" : null}>
+        <div className="homeLeft">
+          <img src={Avatar} alt="Cartoonified image" id="bckImg" />
+          <a href={CV}  target="_blank" className="message">
+            {langChange ? engInfo.home[3] : srbInfo.home[3]}
+          </a>
+        </div>
         <div className="homeRight">
-          <p>Well hello there,</p>
-          <p>My name is Stefan</p>
-          <p>And I like to create wonderful digital things like this.</p>
+          <p>{langChange ? engInfo.home[0] : srbInfo.home[0]}</p>
+          <p>{langChange ? engInfo.home[1] : srbInfo.home[1]}</p>
+          <p>{langChange ? engInfo.home[2] : srbInfo.home[2]}</p>
         </div>
       </main>
     </>
