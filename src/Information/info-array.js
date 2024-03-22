140163eb292556a4f7912dcd51c621d033b2c80d
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-03-22 23:14:14 +0100

    Add new entries into an array.

--- src/Information/info-array.js
+++ src/Information/info-array.js
@@ -6,7 +6,13 @@ const engInfo = {
     "And I love to create wonderful digital things like this.",
     "CV click here",
   ],
-  about: [],
+  about: [
+    "As you already saw, my name is Stefan about a year ago, I started studying front-end developement. Enthusiasm for this topic reached its zenith when I enrolled in the Comtrade CODE academy that has it's headquarters here in Belgrade. Ever since I've completed the course, I've been working on my resume in the spare time that I've got.",
+    "I also have an experience with digital marketing, having been volunteered on a project regarding mental health awareness. My role in the project was making visual content.",
+    "Otherwise I'm an avid nature lover, that is at home in the forest.",
+    "P.S. If you haven't noticed there is a CV file hidden on an avatar inside the Home page.",
+  ],
+  aboutImg: "This is non cartonized me.",
   skills: [],
   contact: [],
 };
@@ -19,7 +25,13 @@ const srbInfo = {
     "I volim da pravim lepe digitalne kreacije kao što je ova.",
     "CV kilikni ovde",
   ],
-  about: [],
+  about: [
+    "Kao što ste već mogli videti, moje ime je Stefan, pre odprilke godinu dana sam počeo sa učenjem front-end developmenta. Entuzijazam za ovom temom je dostigao svoj vrhunac kada sam počeo da pohađam Comtrade akademiju koja ima svoje sedište ovde u Beogradu. Od kada sam završio obuku, radim na svom portfoliu kada god imam slobodnog vremena za to.",
+    "Takođe imam iskustvo u digitalnom marketingu, pošto sam volontirao na projektu u vezi sa svešću o mentalnom zdravlju. Moja uloga u projektu bila je pravljenje vizuelnog sadržaja.",
+    "Inače sam strastveni zaljubljenik u prirodu, koji je u šumi kao kod svoje kuće.",
+    "P.S. Za slučaj da niste primetili, moj CV file se nalazi skriven unutar avatara u Home stranici.",
+  ],
+  aboutImg: "Ovo sam ja u ne nacrtanoj verziji.",
   skills: [],
   contact: [],
 };
