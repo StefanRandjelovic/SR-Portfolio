57c7ad4c81fd94221f129c7763cdfa621fb8d4af
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-03-24 23:01:58 +0100

    Add descriptions and darkBackground helper function.

--- src/Helper_functions/helpers.js
+++ src/Helper_functions/helpers.js
@@ -1,3 +1,4 @@
+// TEXT COPYING AND LINKING - Contact.jsx
 const handleCopyText = (event, link, elem) => {
   if (!event.ctrlKey) {
     navigator.clipboard.writeText(link);
@@ -13,6 +14,7 @@ const handleCopyText = (event, link, elem) => {
   }
 };
 
+// TEXT COPYING - Contact.jsx
 const handleOnlyCopy = (mail, elem) => {
   navigator.clipboard.writeText(mail);
   elem.style.opacity = "1";
@@ -23,4 +25,15 @@ const handleOnlyCopy = (mail, elem) => {
   }, 2000);
 };
 
-export { handleCopyText, handleOnlyCopy };
+// BACKGROUND IMG CHANGER - NavBar.jsx
+const darkBackground = (darkModeState) => {
+  if (darkModeState) {
+    document.documentElement.style.backgroundImage =
+      "url(/src/Images/backgroundDark.png)";
+  } else {
+    document.documentElement.style.backgroundImage =
+      "url(/src/Images/background.png)";
+  }
+};
+
+export { handleCopyText, handleOnlyCopy, darkBackground };
