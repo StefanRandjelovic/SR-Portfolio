f2433f40d918c87e119fef74902154a9b23035f5
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-03-24 20:47:38 +0100

    Add dynamic content into the Contacts page and add two helper functions.

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -10,6 +10,9 @@ import { useAtomValue } from "jotai";
 // GSVariable
 import { languageChange, darkMode } from "@global/store.js";
 
+// HELPER FUNCTIONS
+import { handleCopyText, handleOnlyCopy } from "@helpers/helpers";
+
 const Contact = () => {
   // GSVariable
   const darkModeState = useAtomValue(darkMode);
@@ -22,18 +25,77 @@ const Contact = () => {
           ? engInfo.nav[engInfo.nav.length - 1]
           : srbInfo.nav[srbInfo.nav.length - 1]}
       </h1>
-      <p>
+      <div className="message">
+        <p>
+          {langChange ? engInfo.contact.message[0] : srbInfo.contact.message[0]}
+        </p>
+        <p>
+          {langChange ? engInfo.contact.message[1] : srbInfo.contact.message[1]}
+        </p>
+      </div>
+
+      <p
+        title={langChange ? "Click to copy" : "Kliknite za kopiranje"}
+        id="mail"
+        onClick={() =>
+          handleOnlyCopy(
+            engInfo.contact.email[1],
+            document.querySelector("#copied")
+          )
+        }
+      >
         <span>
           {langChange ? engInfo.contact.email[0] : srbInfo.contact.email}
         </span>
         {` ${engInfo.contact.email[1]}`}
       </p>
-      <p>
+      <p
+        title={
+          langChange
+            ? "Click to copy, Ctrl + Click to follow link"
+            : "Kliknite za kopiranje, Ctrl + klik da pratite link"
+        }
+        id="git"
+        onClick={(event) =>
+          handleCopyText(
+            event,
+            engInfo.contact.gitHub[1],
+            document.querySelector("#copied")
+          )
+        }
+      >
         <span>
           {langChange ? engInfo.contact.gitHub[0] : srbInfo.contact.gitHub}
         </span>
         {` ${engInfo.contact.gitHub[1]}`}
       </p>
+      <div className="socialM">
+        <p>
+          {langChange
+            ? engInfo.contact.socialMedia.socialM
+            : srbInfo.contact.socialMedia.socialM}
+        </p>
+        <p
+          title={
+            langChange
+              ? "Click to copy, Ctrl + Click to follow link"
+              : "Kliknite za kopiranje, Ctrl + klik da pratite link"
+          }
+          id="linkedIn"
+          onClick={(event) =>
+            handleCopyText(
+              event,
+              engInfo.contact.socialMedia.linkedIn[1],
+              document.querySelector("#copied")
+            )
+          }
+        >
+          <span>{engInfo.contact.socialMedia.linkedIn[0]}</span>
+
+          {` ${engInfo.contact.socialMedia.linkedIn[1]}`}
+        </p>
+      </div>
+      <p id="copied">COPIED!</p>
     </main>
   );
 };
