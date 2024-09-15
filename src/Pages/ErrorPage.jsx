fe77f62a55d9df8ad0b03cf83b13b49e51c9a980
StefanRandjelovic <stefanrandjelovic413@gmail.com>
2024-09-15 17:29:44 +0200

    Create ErrorPage to handle invalid links paths.

---
+++ src/Pages/ErrorPage.jsx
@@ -0,0 +1,14 @@
+// STYLES
+import "@styles/ErrorPage.scss";
+
+const ErrorPage = () => {
+  return (
+    <>
+      <main id="error_page">
+        <p>Could not found this page</p>
+      </main>
+    </>
+  );
+};
+
+export default ErrorPage;
