0bb87d2214fc64dec74f66d57e3a922fbdf6605d
StefanRandjelovic <stefanrandjelovic413@gmail.com>
2024-09-15 18:22:50 +0200

    Finish building ErrorPage.

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,11 +1,22 @@
 // STYLES
 import "@styles/ErrorPage.scss";
 
+// INFO
+import { srbInfo, engInfo } from "@info/info-array";
+
+// DEV DEPENDENCIES
+import { useAtomValue } from "jotai";
+
+// GSVariable
+import { languageChange } from "@global/store.js";
+
 const ErrorPage = () => {
+  const langChange = useAtomValue(languageChange);
+
   return (
     <>
       <main id="error_page">
-        <p>Could not found this page</p>
+        <h1>{langChange ? engInfo.errorPage : srbInfo.errorPage}</h1>
       </main>
     </>
   );
