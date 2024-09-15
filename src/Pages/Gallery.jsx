707f2a5ebe09d0a7939fad9ae8c3c081434cc694
StefanRandjelovic <stefanrandjelovic413@gmail.com>
2024-09-15 18:36:36 +0200

    Create Gallery page.

---
+++ src/Pages/Gallery.jsx
@@ -0,0 +1,15 @@
+const Gallery = () => {
+  return (
+    <>
+      <main id="gallery">
+        <section>
+          <button id="back">back</button>
+          <img src="" alt="" />
+          <button id="forward">fwd</button>
+        </section>
+      </main>
+    </>
+  );
+};
+
+export default Gallery;
