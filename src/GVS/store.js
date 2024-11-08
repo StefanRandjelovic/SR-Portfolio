1a6cfe296b382a2b5bc0f4f404ef0f752ed16335
StefanRandjelovic <stefanrandjelovic413@gmail.com>
2024-11-08 19:28:07 +0100

    Change the initial value of darkMode.

--- src/GVS/store.js
+++ src/GVS/store.js
@@ -3,6 +3,6 @@ import { atomWithStorage } from "jotai/utils";
 
 const languageChange = atomWithStorage("language", false);
 
-const darkMode = atomWithStorage("dark-mode", false);
+const darkMode = atomWithStorage("dark-mode", true);
 
 export { languageChange, darkMode };
